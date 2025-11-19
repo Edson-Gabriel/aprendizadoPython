ff69d9d9ee26400504fbe63935ee80dbd67ea04d
Edson Gabriel de Souza Hoffmann <egabriel.hoffmann@gmail.com>
2025-11-18 20:25:31 -0400

    Remove pasta Aulas do repositório e adiciona ao .gitignore

--- aulas/aula18.py
+++
@@ -1,8 +0,0 @@
-primeiro_valor = input('Digite um valor: ')
-segundo_valor = input('Digite um valor: ')
-if primeiro_valor > segundo_valor:
-    print(f'o {primeiro_valor=} é maior que o {segundo_valor=}')
-elif primeiro_valor < segundo_valor:
-    print(f'o {segundo_valor=} é maior que o {primeiro_valor=}')
-else:
-    print('Os valores são iguais')
