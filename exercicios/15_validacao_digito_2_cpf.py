a5a846e99a37cf76d4b43eb9ddf563753c39ce32
Edson Gabriel de Souza Hoffmann <egabriel.hoffmann@gmail.com>
2025-11-24 21:45:25 -0400

    Resolução das atividades de validação dos dígitos do cpf

--- exercicios/15_validacao_digito_2_cpf.py
+++ exercicios/15_validacao_digito_2_cpf.py
@@ -1,4 +1,29 @@
 """
+Calculo do primeiro dígito do CPF
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF
+multiplicando cada um dos valores por uma
+contagem regressiva começando de 10
+
+Ex.:  746.824.890-70 (746824890)
+   10  9  8  7  6  5  4  3  2
+*  7   4  6  8  2  4  8  9  0
+   70  36 48 56 12 20 32 27 0
+
+Somar todos os resultados: 
+70+36+48+56+12+20+32+27+0 = 301
+Multiplicar o resultado_digito_1 anterior por 10
+301 * 10 = 3010
+Obter o resto da divisão da conta anterior por 11
+3010 % 11 = 7
+Se o resultado_digito_1 anterior for maior que 9:
+    resultado_digito_1 é 0
+contrário disso:
+    resultado_digito_1 é o valor da conta
+
+O primeiro dígito do CPF é 7
+"""
+"""
 Calculo do segundo dígito do CPF
 CPF: 746.824.890-70
 Colete a soma dos 9 primeiros dígitos do CPF,
@@ -25,14 +50,27 @@ contrário disso:
 O segundo dígito do CPF é 0
 """
 
-cpf = cpf = '746824890'
-nove_digitos = cpf[:10]
-contador_regressivo_2 = 11
+
+cpf = '74682489070'
+nove_digitos = cpf[:9]
+contador_regressivo_1 = 10
 
 resultado_digito_1 = 0
-for digito_2 in nove_digitos:
-    resultado_digito_1 += int(digito_2) * contador_regressivo_1
+for digito_1 in nove_digitos:
+    resultado_digito_1 += int(digito_1) * contador_regressivo_1
     contador_regressivo_1 -= 1
-digito_2 = (resultado_digito_1 * 10) % 11
+digito_1 = (resultado_digito_1*10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+print(digito_1)
+
+dez_digitos = nove_digitos + str(digito_1)
+dez_digitos = dez_digitos[:10]
+
+contador_regressivo_2 = 11
+resultado_digito_2 = 0
+for digito_2 in dez_digitos:
+    resultado_digito_2 += int(digito_2) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = (resultado_digito_2*11) %11
 digito_2 = digito_2 if digito_2 <= 9 else 0
 print(digito_2)
